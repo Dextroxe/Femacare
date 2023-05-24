a45f8dc45d2a18ddc580d6f8b71c267baa611896
DExTROx <dashmathembram99@gmail.com>
2023-05-24 21:25:31 +0530

    lower the typescript strict

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,15 +1,9 @@
 import {Route,Routes} from 'react-router-dom'
 import App from './App'
-import {Explore,Enroll} from './pages/index.js'
+import {Explore,Enroll} from './pages'
 
 
-window.addEventListener('phx:page-loading-stop', (e) => {
-    // trigger flowbite events
-    window.document.dispatchEvent(new Event("DOMContentLoaded", {
-      bubbles: true,
-      cancelable: true
-    }));
-  });
+
 
 function Routers()  {
     return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
                         <Dropdown.Header className="w-full" >
                             <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Login</button>
                         </Dropdown.Header>
-                        <Dropdown.Item icon={HiChatAlt2}>
+                        <Dropdown.Item icon={HiChatAlt2} >
                             Blogs
                         </Dropdown.Item>
                         <Dropdown.Item icon={BiBook}>
